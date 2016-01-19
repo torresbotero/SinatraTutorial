1b63373a576eaa2496f1fe64fbb15d9f89b38b7e
Camilo Torres Botero <camilo.torresbotero@gmail.com>
2016-01-18 23:27:19 -0500

    First Commit

---
+++ basics.rb
@@ -0,0 +1,39 @@
+require 'rubygems'
+require 'sinatra'
+
+get '/' do
+	"Hello, World!"
+end
+
+get '/about' do
+  'A little about me.'
+end
+
+get '/hello/:name' do
+  "Hello there, #{params[:name]}."
+end
+
+get '/form' do
+  erb :form
+end
+
+post '/form' do
+  "You said '#{params[:message]}'"
+end
+
+get '/secret' do
+  erb :secret
+end
+
+post '/secret' do
+  params[:secret].reverse
+end
+
+get '/decrypt/:secret' do
+  params[:secret].reverse
+end
+
+not_found do
+  status 404
+  'not found'
+end
